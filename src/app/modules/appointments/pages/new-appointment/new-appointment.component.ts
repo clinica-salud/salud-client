408719111ffcbba21eb5add87f2873c27d9531be
Yobel <rotkyobel@hw4nb.com>
2024-03-27 22:31:50 -0500

    update

--- src/app/modules/appointments/pages/new-appointment/new-appointment.component.ts
+++ src/app/modules/appointments/pages/new-appointment/new-appointment.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+
+import { NbCardModule } from '@nebular/theme'
+
+const NB_MODULES = [NbCardModule]
 
 @Component({
-  selector: 'app-new-appointment',
-  standalone: true,
-  imports: [],
-  templateUrl: './new-appointment.component.html',
-  styleUrl: './new-appointment.component.scss'
+	selector: 'app-new-appointment',
+	standalone: true,
+	imports: [...NB_MODULES],
+	templateUrl: './new-appointment.component.html',
+	styleUrl: './new-appointment.component.scss'
 })
-export class NewAppointmentComponent {
-
-}
+export class NewAppointmentComponent {}
