24f565f41f1e411a8d0f822f2149d5d70adef3c9
Yobel <rotkyobel@hw4nb.com>
2024-04-02 21:37:58 -0500

    feat: add login and register page

--- src/app/core/auth/auth.component.ts
+++ src/app/core/auth/auth.component.ts
@@ -1,10 +1,102 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { NbCardModule, NbLayoutModule } from '@nebular/theme';
+
+import { BackgroundComponent } from '@src/app/core/auth/components/background/background.component';
+import { LogoNameComponent } from '@src/app/core/auth/components/logo-name/logo-name.component';
+
+const NB_MODULES = [NbLayoutModule, NbCardModule];
+const COMPONENTS = [BackgroundComponent, LogoNameComponent];
+
 @Component({
 	selector: 'app-auth',
 	standalone: true,
-	imports: [RouterOutlet],
-	template: '<router-outlet />'
+	imports: [RouterOutlet, ...NB_MODULES, ...COMPONENTS],
+	template: `
+		<nb-layout>
+			<nb-layout-column class="p-0">
+				<div class="main-container">
+					<app-background class="images" />
+					<div class="form">
+						<nb-card class="p-0 mb-0">
+							<nb-card-body class="px-5">
+								<app-logo-name />
+								<div class="mt-5">
+									<router-outlet />
+								</div>
+							</nb-card-body>
+						</nb-card>
+					</div>
+				</div>
+			</nb-layout-column>
+		</nb-layout>
+	`,
+	styles: `
+		.main-container {
+			display: grid;
+			grid-template-columns: repeat(9, 1fr);
+			min-height: 100dvh;
+
+			.images {
+				grid-column: 1 / span 6;
+			}
+
+			.form {
+				grid-column: 7 / span 3;
+			}
+
+			nb-card {
+				border: none;
+				height: 100vh;
+			}
+
+			nb-card-body {
+				padding-block: 4rem;
+			}
+
+			@media screen and (max-width: 1200px) {
+				.images {
+					grid-column: 1 / span 5;
+				}
+
+				.form {
+					grid-column: 6 / span 4;
+				}
+			}
+
+			@media screen and (max-width: 1024px) {
+				.images {
+					grid-column: 1 / span 4;
+				}
+
+				.form {
+					grid-column: 5 / span 5;
+				}
+			}
+
+			@media screen and (max-width: 768px) {
+				.images {
+					display: none;
+				}
+
+				.form {
+					grid-column: 1 / span 9;
+				}
+			}
+
+			@media screen and (max-width: 768px) {
+				.main-container {
+					align-content: center;
+				}
+			}
+
+			@media screen and (max-height: 800px) {
+				nb-card-body {
+					padding-block: 2rem;
+				}
+			}
+
+		}`
 })
 export class AuthComponent {}
--- src/app/core/auth/pages/register/register.component.ts
+++ src/app/core/auth/pages/register/register.component.ts
@@ -3,28 +3,38 @@ import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } 
 import { Router, RouterLink } from '@angular/router';
 
 import { NbEvaIconsModule } from '@nebular/eva-icons';
-import { NbButtonModule, NbCheckboxModule, NbIconModule, NbInputModule } from '@nebular/theme';
+import { NbButtonModule, NbIconModule, NbInputModule, NbSelectModule } from '@nebular/theme';
 
 import { ControlErrorComponent } from '@src/app/shared/helpers/control-error/control-error.component';
 
-const NB_MODULES = [NbIconModule, NbInputModule, NbButtonModule, NbEvaIconsModule, NbCheckboxModule];
+const NB_MODULES = [NbIconModule, NbInputModule, NbButtonModule, NbEvaIconsModule, NbSelectModule];
 const COMPONENTS = [ControlErrorComponent];
 
 @Component({
-	selector: 'app-login',
+	selector: 'app-register',
 	standalone: true,
 	imports: [ReactiveFormsModule, RouterLink, ...NB_MODULES, ...COMPONENTS],
-	templateUrl: './login.component.html',
-	styleUrls: ['./login.component.scss']
+	templateUrl: './register.component.html',
+	styleUrl: './register.component.scss'
 })
-export class LoginComponent {
+export class RegisterComponent {
 	private _fb = inject(FormBuilder);
 	private _router = inject(Router);
 
 	public form: FormGroup = this._fb.group({
+		doc_type: ['', [Validators.required]],
+		doc_number: ['', [Validators.required]],
 		email: ['', [Validators.required, Validators.email]],
 		password: ['', [Validators.required, Validators.minLength(6)]]
 	});
+
+	get doc_type(): FormControl {
+		return this.form.get('doc_type') as FormControl;
+	}
+
+	get doc_number(): FormControl {
+		return this.form.get('doc_number') as FormControl;
+	}
 
 	get email(): FormControl {
 		return this.form.get('email') as FormControl;
